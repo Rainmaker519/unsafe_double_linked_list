45f0e1cfb9d475463e53d3ff7ee73884f22a4482
Rainmaker519 <rainmaker519@gmail.com>
2024-02-18 13:45:45 -0500

    Got it working, might be somewhat unsafe gotta put some more robust tests in place to really check.

--- src/main.rs
+++ src/main.rs
@@ -23,23 +23,39 @@ fn main() {
     let mut list = DLL {
         nodes: vec![],
         len: 0,
+        head: Node {
+            val: 0.0f64,
+            next: Option::None,
+            prev: Option::None
+        },
+        tail: &mut Node {
+            val: 0.0f64,
+            next: Option::None,
+            prev: Option::None
+        },
     };
     list.add(n1);
     list.add(n2);
     list.add(n3);
 
     list.set(0, 21.22f64);
-    list.set(01, 1f64);
+    list.set(1, 1f64);
     list.set(2, 11.11f64);
 
     println!("{:?}",list.get(0));
     println!("{:?}",list.get(1));
     println!("{:?}",list.get(2));
+    println!();
 
     list.remove(1);
 
     println!("{:?}",list.get(0));
     println!("{:?}",list.get(1));
+    println!();
+
+    list.remove(1);
+
+    println!("{:?}",list.get(0));
 }
 
 
@@ -48,36 +64,49 @@ struct DLL {
     nodes: Vec<Node>,
     #[derivative(Default(value = "0"))]
     len: usize,
+    head: Node,
+    tail: *mut Node,
 }
 impl DLL {
-    fn add(&mut self, node: Node) {
+    fn add(&mut self, mut node: Node) {
+        //if there is only one node, tail will stay as none
         if self.len <= 0 {
-            self.nodes.push(node);
+            self.head = node;
             self.len = 1;
         }
         else {
             let len = self.len;
 
-            if len != self.nodes.len() {
-                panic!("Len incorrect.");
+            if len == 1 {
+                let old_last = &mut self.head as *mut Node;
+                let new_last = &mut node as *mut Node;
+
+                unsafe {
+                    set_next(&mut *old_last, Option::Some(&mut *new_last));
+                    set_prev(&mut *new_last, Option::Some(&mut *old_last));
+                }
+
+                self.tail = new_last;
+                self.len += 1;
             }
+            else {
+                let old_last = self.tail;
+                let new_last = &mut node as *mut Node;
 
-            self.nodes.push(node);
+                unsafe {
+                    set_next(&mut *old_last, Option::Some(&mut *new_last));
+                    set_prev(&mut *new_last, Option::Some(&mut *old_last));
 
-            let old_last = &mut self.nodes[len-1] as *mut Node;
-            let new_last = &mut self.nodes[len] as *mut Node;
+                    self.tail = new_last;
+                }
 
-            unsafe {
-                set_next(&mut *old_last, Option::Some(&mut *new_last));
-                set_prev(&mut *new_last, Option::Some(&mut *old_last));
+                self.len += 1;
             }
-
-            self.len += 1;
         }
     }
 
     fn get(&self, ind: usize) -> f64 {
-        let mut node_ptr = &self.nodes[0];
+        let mut node_ptr = &self.head;
         if ind != 0 {
             for i in 0..ind {
                 match get_next(node_ptr) {
@@ -91,8 +120,23 @@ impl DLL {
         return get_value(node_ptr);
     }
 
+    fn get_mut_ptr(&mut self, ind: usize) -> *mut Node {
+        let mut node_ptr = &mut self.head;
+        if ind != 0 {
+            for i in 0..ind {
+                match get_next_mut(node_ptr) {
+                    None => { panic!("In DLL get, tried to get next on option::none"); }
+                    Some(node) => {
+                        node_ptr = node;
+                    }
+                }
+            }
+        }
+        return node_ptr;
+    }
+
     fn set(&mut self, ind: usize, val: f64) {
-        let mut node_ptr = &self.nodes[0];
+        let mut node_ptr = &self.head;
         let mut_node_ptr: &mut Node;
 
         if ind > 1 {
@@ -106,7 +150,7 @@ impl DLL {
             }
         }
         if ind == 0 {
-            mut_node_ptr = &mut self.nodes[0];
+            mut_node_ptr = &mut self.head;
         }
         else {
             match get_next_mut(node_ptr) {
@@ -128,29 +172,38 @@ impl DLL {
         }
 
         if ind == self.len-1 {
-            set_next(&mut self.nodes[ind-1], Option::None);
-            self.nodes.remove(ind);
+            unsafe {
+                let tail_ref = get_prev_mut(&*self.tail).unwrap();
+                set_next(tail_ref,Option::None);
+                self.tail = tail_ref as *mut Node;
+            }
+            self.len -= 1;
             return;
         }
         else if ind == 0 {
-            set_prev(&mut self.nodes[1], Option::None);
-            self.nodes.remove(0);
+            let mut next = get_next_mut(&self.head).unwrap();
+            self.head = next.clone();
+            set_prev(&mut self.head, Option::None);
+            self.len -= 1;
             return;
         }
 
-        let mut node_ptr_pre = &mut self.nodes[ind-1] as *mut Node;
-        let mut node_ptr_post = &mut self.nodes[ind+1] as *mut Node;
-        self.nodes.remove(ind);
+
+        let mut node_ptr_pre = self.get_mut_ptr(ind-1);
         unsafe {
+            let mut node_ptr_post = self.get_mut_ptr(ind+1);
+
             let pre = &mut *(node_ptr_pre);
             let post = &mut *(node_ptr_post);
             set_next(pre,Option::Some(post));
             set_prev(post,Option::Some(pre));
         }
+        self.len -= 1;
 
     }
 }
 
+#[derive(Clone)]
 struct Node {
     val: f64,
     next: Option<*mut Node>,
@@ -189,10 +242,12 @@ fn set_next(node: &mut Node, next: Option<&mut Node>) {
     if none {
         node.next = Option::None;
     }
-    let ptr = next.unwrap() as *mut Node;
+    else {
+        let ptr = next.unwrap() as *mut Node;
 
-    unsafe {
-        node.next = Option::Some(ptr);
+        unsafe {
+            node.next = Option::Some(ptr);
+        }
     }
 }
 fn get_prev(node: &Node) -> Option<&Node> {
@@ -227,10 +282,12 @@ fn set_prev(node: &mut Node, prev: Option<&mut Node>) {
     if none {
         node.prev = Option::None;
     }
-    let ptr = prev.unwrap() as *mut Node;
+    else {
+        let ptr = prev.unwrap() as *mut Node;
 
-    unsafe {
-        node.prev = Option::Some(ptr);
+        unsafe {
+            node.prev = Option::Some(ptr);
+        }
     }
 }
 fn get_value(node: &Node) -> f64 {
